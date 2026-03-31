9f4b35bdef584b83edcc2a97bf34ccd660e0a4ea
eshy-m <ehuallpay@gmail.com>
2026-03-31 16:55:30 -0500

    Agregando ruta para seeders

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/crear-admin', function () {
+    \Illuminate\Support\Facades\Artisan::call('db:seed', ['--force' => true]);
+    return '¡Semillas plantadas! Base de datos poblada con éxito.';
 });
 // Route::get('/instalar-bd', function () {
 //     \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
