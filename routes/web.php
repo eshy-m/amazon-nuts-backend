1e0932ffdee3c89fa899013337537dbf346f9fc6
eshy-m <ehuallpay@gmail.com>
2026-03-30 23:16:35 -0500

    Agregando ruta secreta para instalar BD

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/instalar-bd', function () {
+    \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
+    return '¡Tablas creadas con éxito, eres un hacker!';
 });
