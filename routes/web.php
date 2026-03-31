9e2fdc5077c608fa1eaf4d8eb605b7e136949dbf
eshy-m <ehuallpay@gmail.com>
2026-03-31 17:08:07 -0500

    Actualizando y creando usuario y contraseña

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/crear-admin-seguro', function () {
+    \App\Models\User::updateOrCreate(
+        ['email' => 'admin@amazonnuts.com'],
+        [
+            'name' => 'Administrador',
+            'password' => \Illuminate\Support\Facades\Hash::make('tu_contraseña_aqui')
+        ]
+    );
+    return 'Usuario admin@amazonnuts.com creado o actualizado con éxito.';
 });
 // Route::get('/instalar-bd', function () {
 //     \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
 //     return '¡Tablas creadas con éxito, eres un hacker!';
+// });
+
+//como crear usuario y contraseña
+// Route::get('/crear-admin-seguro', function () {
+//     \App\Models\User::updateOrCreate(
+//         ['email' => 'admin@amazonnuts.com'],
+//         [
+//             'name' => 'Administrador',
+//             'password' => \Illuminate\Support\Facades\Hash::make('tu_contraseña_aqui')
+//         ]
+//     );
+//     return 'Usuario admin@amazonnuts.com creado o actualizado con éxito.';
 // });
