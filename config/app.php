105327013c3cfc0ef6dab54d3668794b8bdc7c1c
eshy-m <ehuallpay@gmail.com>
2026-04-08 10:28:27 -0500

    actualizando backend

--- config/app.php
+++ config/app.php
@@ -69,7 +69,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Application Locale Configuration
+    | Application Locale Configuracion
     |--------------------------------------------------------------------------
     |
     | The application locale determines the default locale that will be used
